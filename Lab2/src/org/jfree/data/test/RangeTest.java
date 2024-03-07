41112a071300104f40d473d3244aa5d5eb33059e
orlahiggins <orlahiggins@localhost>
2024-03-07 13:04:02 +0000

    Updates for testing - Orla Higgins

---
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,37 @@
+package org.jfree.data.test;
+
+//import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.jfree.data.Range;
+
+
+public class RangeTest {
+	
+	private Range rangeObjectUnderTest;
+
+	@Before
+	public void setUp() throws Exception {
+		rangeObjectUnderTest = new Range(-1, 1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCentralValueShouldBeZero() {
+
+		assertEquals("The central value of -1 and 1 should be 0",
+
+		0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
+
+		}
+
+		}
+
